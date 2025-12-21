409c5335434394869faec3bfc793f1e3b04949c7
sebastian tovar <sebastiantovarchavez304@gmail.com>
2025-12-20 23:56:21 -0500

    feat: :sparkles: Agregar configuración de imágenes y soporte para patrones remotos en next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    // Use remotePatterns to allow images from specific domains and paths
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.thesimpsonsapi.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+  productionBrowserSourceMaps: false,
+  turbopack: {},
 };
 
 export default nextConfig;
