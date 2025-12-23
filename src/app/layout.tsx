e75e07771efccd145a5f33b1877204b723c62a68
sebastian tovar <sebastiantovarchavez304@gmail.com>
2025-12-22 23:34:11 -0500

    feat: :sparkles: Agregar layout raíz para la aplicación de Los Simpsons y tipografia  comic_neue

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+// src/app/layout.tsx
+import type { Metadata } from 'next';
+import '@/globals.css';
+import Header from '@/sections/components/Header';
+import { Comic_Neue } from 'next/font/google';
+
+export const metadata: Metadata = {
+  title: 'The Simpsons Explorer',
+  description: 'Explora personajes, episodios y locaciones de Los Simpsons',
+};
+// Configuración de la fuente Comic Neue
+export const comic = Comic_Neue({
+  subsets: ['latin'],
+  weight: ['700'],    
+  style: ['italic'],      
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="es">
+      <body className={comic.className}>
+        <Header />
+        <main className="container">
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
